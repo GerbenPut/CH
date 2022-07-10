11476d652526eaaf7ec56140da65d23623d30e19
Gerben Put <gerbenputprivate@gmail.com>
2022-07-10 23:02:56 +0200

    initial push

---
+++ resources/views/Misfits/misfits.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.master')
+@section('content')  
+
+<section>
+    <h1>Login</h1>
+    <input type="text">
+</section>
+<section>
+    <h1>Attends</h1>
+    <p>Temari: 10</p>
+    <p>DeadlyLotus: 8</p>
+</section>
+<section>
+    <h1>Timers</h1>
+    <p>Timer: 1</p>
+    <p>Timer: 2</p>
+    <p>Timer: 3</p>
+    <p>Timer: 4</p>
+</section>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Celtic Heroes Bot</title>
+        <meta name="description" content="A bot written for the Celtic Heroes game.">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
