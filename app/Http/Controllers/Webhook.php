c791b03a92cfc2f401d7905137465e470607d79a
Gerben Put <gerbenputprivate@gmail.com>
2022-07-12 18:20:37 +0200

    testing bug

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -12,12 +12,26 @@ require '../vendor/autoload.php';
 class Webhook extends Controller
 {
     public function index() {
+        // get request body and line signature header
+        $body 	   = file_get_contents('php://input');
+        $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+        // log body and signature
+        file_put_contents('php://stderr', 'Body: '.$body);
+
+        // is LINE_SIGNATURE exists in request header?
+        if (empty($signature)){
+            return $response->withStatus(400, 'Signature not set');
+        }
+
+        // is this request comes from LINE?
+        if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, '285fbf204bce13bc0c60c5c4bf28c8d2', $signature)){
+            return $response->withStatus(400, 'Invalid signature');
+        }
+
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
-
-        $body 	   = file_get_contents('php://input');
-	    $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
-        $data = json_decode($body, true);
+        $data = json_decode($body, true);   
 
         foreach ($data['events'] as $event) {
             // $userMessage = $event['message']['text'];
