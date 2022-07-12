fa44b5263b4e88d1bc6c683ac9537807c47aa4ff
Gerben Put <gerbenputprivate@gmail.com>
2022-07-12 21:52:00 +0200

    added reset timer

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -64,7 +64,7 @@ class Webhook extends Controller
 
             if ($pieces[0] == "timers") {
                 $lines = BossTimer::all()
-                ->map(fn (BossTimer $timer) => sprintf('%s spawns in %s', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans()));
+                ->map(fn (BossTimer $timer) => sprintf('%s opens in %s and closes in ', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans(),$timer->date->addMinutes($timer->closed)->diffForHumans()));
                 
                 $message = $lines->isEmpty()
                     ? 'No timers.'
