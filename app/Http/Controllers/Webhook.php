2e0259a0c97f396d1fd2c36f76da89e7acdeea9d
Gerben Put <gerbenputprivate@gmail.com>
2022-07-12 18:16:55 +0200

    testing bug

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+// use Psr\Http\Message\ResponseInterface as Response;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+require '../vendor/autoload.php';
 
 class Webhook extends Controller
 {
