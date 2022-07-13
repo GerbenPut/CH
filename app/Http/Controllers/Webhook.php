a649927993e1f4a1da842573e8a594f17c04c6a5
Gerben Put <gerbenputprivate@gmail.com>
2022-07-13 02:31:50 +0200

    new feature

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -8,72 +8,6 @@ use App\Models\BossTimer;
 class Webhook extends Controller
 {
     public function index(Request $request) {
-        function raidTimers($pieces) {
-            if ($pieces[0] == "timers") {
-                $lines = BossTimer::all()->where('type', $pieces[1])
-                ->map(fn (BossTimer $timer) => sprintf('%s opens: %s and closes: %s', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans(null, true),$timer->date->addMinutes($timer->closed)->diffForHumans(null, true)));
-                
-                $message = $lines->isEmpty()
-                    ? 'No timers.'
-                    : $lines->join("\n");
-            } else if ($pieces[0] == "reset") {
-                $timer = BossTimer::query()->firstWhere('name', $pieces[1]);
-    
-                if ($timer === null) {
-                    $message = "Boss not found!";
-                } else {
-                    $timer->date = now();
-                    $timer->save();
-                    $message = $pieces[1] . " has been reset!";
-                }
-            } else {
-                $message = "Command not found.";
-            }
-        }
-    
-        function timers($pieces) {
-            if ($pieces[0] == "timers") {
-                $raids = array("necromancer", "proteus", "gelebron", "dhiothu", "bloodthorn", "hrungnir");
-                if (in_array($pieces[1], $raids)) {
-                    $message = "You are not allowed to that boss yet. *insert evil smiley*";
-                } else {
-                    $lines = BossTimer::all()->where('type', $pieces[1])
-                    ->map(fn (BossTimer $timer) => sprintf('%s opens: %s and closes: %s', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans(null, true),$timer->date->addMinutes($timer->closed)->diffForHumans(null, true)));
-                    
-                    $message = $lines->isEmpty()
-                        ? 'No timers.'
-                        : $lines->join("\n");
-                }
-            } else if ($pieces[0] == "reset") {
-                if (in_array($pieces[1], $raids)) {
-                    $message = "You are not allowed to that boss yet. *insert evil smiley*";
-                } else {
-                    $timer = BossTimer::query()->firstWhere('name', $pieces[1]);
-    
-                    if ($timer === null) {
-                        $message = "Boss not found!";
-                    } else {
-                        $timer->date = now();
-                        $timer->save();
-                        $message = $pieces[1] . " has been reset!";
-                    }
-                }
-            } else {
-                $message = "Command not found.";
-            }
-        }
-    
-        function commands($pieces) {
-            return;
-        }
-    
-        function attends($pieces) {
-            return;
-        }
-    
-        function admin($pieces) {
-            return;
-        }
         // Setup Bot
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
@@ -98,7 +32,35 @@ class Webhook extends Controller
 
             if ($event['source']['groupId'] == "C715a78987e46d1ffde053bda1a4de65c") {
                 // Timers
-                $message = timers($pieces);
+                if ($pieces[0] == "timers") {
+                    $raids = array("necromancer", "proteus", "gelebron", "dhiothu", "bloodthorn", "hrungnir");
+                    if (in_array($pieces[1], $raids)) {
+                        $message = "You are not allowed to that boss yet. *insert evil smiley*";
+                    } else {
+                        $lines = BossTimer::all()->where('type', $pieces[1])
+                        ->map(fn (BossTimer $timer) => sprintf('%s opens: %s and closes: %s', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans(null, true),$timer->date->addMinutes($timer->closed)->diffForHumans(null, true)));
+                        
+                        $message = $lines->isEmpty()
+                            ? 'No timers.'
+                            : $lines->join("\n");
+                    }
+                } else if ($pieces[0] == "reset") {
+                    if (in_array($pieces[1], $raids)) {
+                        $message = "You are not allowed to that boss yet. *insert evil smiley*";
+                    } else {
+                        $timer = BossTimer::query()->firstWhere('name', $pieces[1]);
+        
+                        if ($timer === null) {
+                            $message = "Boss not found!";
+                        } else {
+                            $timer->date = now();
+                            $timer->save();
+                            $message = $pieces[1] . " has been reset!";
+                        }
+                    }
+                } else {
+                    $message = "Command not found.";
+                }
             } else if ($event['source']['groupId'] == "C9c94873e053e9a41bc9e55c1e9c54654") {
                 // Admin
                 $message = admin($pieces);
@@ -110,7 +72,26 @@ class Webhook extends Controller
                 $message = attends($pieces);
             } else if ($event['source']['groupId'] == "C89dae52ca0c01f5c46dd825c2a4eed2d") {
                 // Raid Timers
-                $message = raidTimers($pieces);
+                if ($pieces[0] == "timers") {
+                    $lines = BossTimer::all()->where('type', $pieces[1])
+                    ->map(fn (BossTimer $timer) => sprintf('%s opens: %s and closes: %s', $timer->name, $timer->date->addMinutes($timer->open)->diffForHumans(null, true),$timer->date->addMinutes($timer->closed)->diffForHumans(null, true)));
+                    
+                    $message = $lines->isEmpty()
+                        ? 'No timers.'
+                        : $lines->join("\n");
+                } else if ($pieces[0] == "reset") {
+                    $timer = BossTimer::query()->firstWhere('name', $pieces[1]);
+        
+                    if ($timer === null) {
+                        $message = "Boss not found!";
+                    } else {
+                        $timer->date = now();
+                        $timer->save();
+                        $message = $pieces[1] . " has been reset!";
+                    }
+                } else {
+                    $message = "Command not found.";
+                }
             } else {
                 return;
             }
