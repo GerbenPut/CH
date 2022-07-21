2f9b65f1a77a65dc914e00a47f2f62d3b35d5f7b
Noël Hagestein <6616996+Neol3108@users.noreply.github.com>
2022-07-21 22:09:45 +0200

    Add due filtering

--- app/Support/Commands/TimersCommand.php
+++ app/Support/Commands/TimersCommand.php
@@ -5,6 +5,8 @@ namespace App\Support\Commands;
 use App\Models\BossTimer;
 use Illuminate\Database\Eloquent\Builder;
 use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
 
 class TimersCommand extends Command
 {
@@ -12,6 +14,10 @@ class TimersCommand extends Command
 
     public function handle(array $args, ?string $group): void
     {
+        $advance = isset($args[1]) && ctype_digit($args[1])
+            ? intval($args[1])
+            : null;
+
         if (isset($args[0]) && $args[0] == 'raid' && $group === 'timers') {
             $this->reply('You are not allowed to those bosses yet. *insert evil smiley*');
 
@@ -21,6 +27,13 @@ class TimersCommand extends Command
         $lines = BossTimer::query()
             ->when(isset($args[0]), fn (Builder $builder) => $builder->where('type', $args[0]))
             ->get()
+            ->when($advance !== null, function (EloquentCollection $timers) use ($advance) {
+                return $timers
+                    ->filter(function (BossTimer $timer) use ($advance) {
+                        return $timer->date->addMinutes($timer->open)->subMinutes($advance + 1)->lessThanOrEqualTo(now())
+                            && $timer->date->addMinutes($timer->closed)->greaterThanOrEqualTo(now());
+                    });
+            })
             ->map(function (BossTimer $timer) {
                 return sprintf(
                     '%s | opens: %s - closes: %s',
@@ -28,15 +41,10 @@ class TimersCommand extends Command
                     $this->date($timer->date->addMinutes($timer->open)),
                     $this->date($timer->date->addMinutes($timer->closed)),
                 );
+            })
+            ->whenNotEmpty(function (Collection $lines) {
+                $this->reply($lines->implode("\n"));
             });
-
-        if ($lines->isEmpty()) {
-            $this->reply('Invalid timer type. See notes for options.');
-
-            return;
-        }
-
-        $this->reply($lines->implode("\n"));
     }
 
     private function date(CarbonInterface $date): string
