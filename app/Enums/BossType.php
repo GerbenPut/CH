b302535c904d13683bb79f32e91d54b21fca8afd
Noël Hagestein <6616996+Neol3108@users.noreply.github.com>
2022-09-05 20:56:06 +0200

    Add extra types

--- app/Enums/BossType.php
+++ app/Enums/BossType.php
@@ -7,5 +7,7 @@ enum BossType: string
     case DragonLord = 'dl';
     case ExaltedDragonLord = 'edl';
     case Legacy = 'legacy';
+    case Meteoric = 'meteoric';
     case Raid = 'raid';
+    case RingBoss = 'rb';
 }
